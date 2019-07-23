cfcc622b1d14013ec8f349285d58da9ca6c2e85c
Lanysc <36964927+Lanysc@users.noreply.github.com>
2019-07-23 11:05:02 -0300

    Add files via upload

---
+++ MochilaBasico.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define inf 123456789
+
+int memo[10000][10000], n = 3;
+int p[3] = {10,20,30};
+int v[3] = {60,100,120};
+
+int bag(int i, int disp)
+{
+	if (i == n || disp == 0) return 0;
+	int &pd = memo[i][disp];
+	if (pd != -1) return pd;
+	if (p[i] > disp)
+		return pd = bag(i+1,disp);
+	return pd = max(bag(i+1,disp),v[i] + bag(i+1,disp - p[i]));
+}
+
+int main()
+{
+	memset(memo,-1,sizeof memo);
+	int peso;
+	scanf("%d",&peso);
+	cout << bag(0,peso) << endl;
+	
+}
